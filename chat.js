760e6a412fb5add56d33056f61b603de5329e3ab
maria <marcavmer@alu.edu.gva.es>
2024-06-12 14:33:47 +0200

    first commit

---
+++ chat.js
@@ -0,0 +1,11 @@
+const $ = el => document.querySelector(el)
+
+//pongo delante de la variable un símbolo de $
+// para indicar que es un elemnto del DOM
+
+const $form = $('form')
+const $input = $('input')
+const $template = $('#message-template')
+const $messages = $('ul')
+const $container = $('main')
+const $button = $('button')
